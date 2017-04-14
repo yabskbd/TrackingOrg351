8fa0957e5c6262adc1d4a2a07633305260bee4fa
yabskbd <yabskbd@umich.edu>
2017-04-14 00:45:47 -0400

    Implemented Cross-Correlation (convulation) method with shifting Box window.

--- import_video_starter_code.m
+++ import_video_starter_code.m
@@ -1,37 +1,46 @@
 %% READ THE AVI
 close all
 mov=VideoReader('v4.avi');
+
+minTime = 0;
+now = 0;
+
 %%% X is Column and Y is ROW
 FIRST_TIME = 1;
-BOX_RANGE = 100;
+BOX_RANGE = 40;
 CENTER_ROW = 0;
 CENTER_COL = 0;
 TARGET_RANGE = 40;
+CONV_BOX_BUF = 10;
+v4_dataset = uint8(zeros(mov.Height,mov.Width,mov.Duration));
 
+Centers_found_X = [];
+Centers_found_Y = [];
+i = 1;
 while(hasFrame(mov))
 %% Read Frame
 video = readFrame(mov);
 
+
 %% Apply Filter(gaus)
-%v_gaus =imgaussfilt(video);
-v_gaus = video;
 
 
 %% Track
 %%% Filter out background 
 
 %Might need to edit 68 value
-M =(v_gaus <= 68); %%Mask to find worms(got 68 from imtool) 
+M =(video <= 68); %%Mask to find worms(got 68 from imtool) 
 M = double(M); %% NEEDS TO BE A DOUBLE FOR CONV
 
 
 if(FIRST_TIME == 1)
-    
 %% Recieve Target
     imshow(~M);
     hold on %%What the keep image up so user can select below
     pause;
     [x,y] = ginput(1); %%Takes users input where the pixel worm is
+    x = uint32(x);
+    y = uint32(y);
     hold on
     plot(x,y,'r.','MarkerSize',20) %%Draws redot of where user selected
     close all;
@@ -59,6 +68,13 @@ if(FIRST_TIME == 1)
 
     BOX_COL_LEFT = CENTER_COL - BOX_RANGE;
     BOX_COL_RIGHT = CENTER_COL + BOX_RANGE;
+ % _------------------------------------%   
+    CONV_ROW_TOP =  BOX_ROW_TOP - CONV_BOX_BUF;
+    CONV_ROW_BOTTOM = BOX_ROW_BOTTOM + CONV_BOX_BUF;
+
+    CONV_COL_LEFT = BOX_COL_LEFT - CONV_BOX_BUF;
+    CONV_COL_RIGHT = BOX_COL_RIGHT + CONV_BOX_BUF;
+    
     
     FIRST_TIME = 0;
 end
@@ -71,18 +87,30 @@ end
 
 %% convolution section
 
+%Timing Stuff
+% % tic;
+% % tstart = tic;
+
 %imshow(Target)
-M_conv1 = conv2(M,Target1,'same');
+%%M_Conv_with = M(CONV_ROW_TOP:CONV_ROW_BOTTOM,CONV_COL_LEFT:CONV_COL_RIGHT);
+M_Conv_with = M;
+%imshow(M_Conv_with);
+M_conv1 = conv2(M_Conv_with,Target1,'same');
 M_max1 = max(max(M_conv1(BOX_ROW_TOP:BOX_ROW_BOTTOM,BOX_COL_LEFT:BOX_COL_RIGHT)));
 
-M_conv2 = conv2(M, Target2, 'same');
+M_conv2 = conv2(M_Conv_with, Target2, 'same');
 M_max2 =  max(max(M_conv2(BOX_ROW_TOP:BOX_ROW_BOTTOM,BOX_COL_LEFT:BOX_COL_RIGHT)));
 
-M_conv3 = conv2(M, Target3, 'same');
+M_conv3 = conv2(M_Conv_with, Target3, 'same');
 M_max3 =  max(max(M_conv3(BOX_ROW_TOP:BOX_ROW_BOTTOM,BOX_COL_LEFT:BOX_COL_RIGHT)));
 
-M_conv4 = conv2(M, Target4, 'same');
+M_conv4 = conv2(M_Conv_with, Target4, 'same');
 M_max4 =  max(max(M_conv4(BOX_ROW_TOP:BOX_ROW_BOTTOM,BOX_COL_LEFT:BOX_COL_RIGHT)));
+
+%Timing Stuff
+% % telapsed = toc(tstart) + toc(tstart);
+% % now = toc(tstart);
+% % maxTime = max(now,minTime);
 
 all_max = [M_max1,M_max2,M_max3,M_max4];
 
@@ -112,18 +140,27 @@ Max_idx_Row = Max_idx_Row(1);
 Max_idx_Col = Max_idx_Col(1);
 
 
-shift_col_by =  Max_idx_Col - 100;
-shift_row_by = Max_idx_Row - 100;
+shift_col_by =  Max_idx_Col - BOX_RANGE;
+shift_row_by = Max_idx_Row - BOX_RANGE;
 
-imshow(M(BOX_ROW_TOP:BOX_ROW_BOTTOM,BOX_COL_LEFT:BOX_COL_RIGHT));
-hold on
+%% Display Center dot
+%imshow(M(BOX_ROW_TOP:BOX_ROW_BOTTOM,BOX_COL_LEFT:BOX_COL_RIGHT));
+%imshow(M_conv_final)
 
-plot(Max_idx_Col,Max_idx_Row,'r.','MarkerSize',20);
+%Main Display
+% figure (1)
+% imshow(M);
+% hold on
+% plot(Max_idx_Col+BOX_COL_LEFT,Max_idx_Row+BOX_ROW_TOP,'r.','MarkerSize',20);
+Centers_found_X = [Max_idx_Col+BOX_COL_LEFT,Centers_found_X];
+Centers_found_Y = [Max_idx_Row+BOX_ROW_TOP,Centers_found_Y];
+
+%plot(Max_idx_Col,Max_idx_Row,'r.','MarkerSize',20)
 %the ~ on the M is to make the worms black and the backgorund white
 %imshow(M) %%Binariezed image(shows worms in black)
 
 %box for the next iteration:
-
+%% ReCalculate Center of Next Box
 CENTER_ROW = CENTER_ROW + shift_row_by; 
 CENTER_COL = CENTER_COL + shift_col_by;
 
@@ -133,6 +170,14 @@ BOX_ROW_BOTTOM = CENTER_ROW + BOX_RANGE;
 
 BOX_COL_LEFT = CENTER_COL - BOX_RANGE;
 BOX_COL_RIGHT = CENTER_COL + BOX_RANGE;
+
+% ------------------------------------------------ %
+
+CONV_ROW_TOP =  BOX_ROW_TOP - CONV_BOX_BUF;
+CONV_ROW_BOTTOM = BOX_ROW_BOTTOM + CONV_BOX_BUF;
+
+CONV_COL_LEFT = BOX_COL_LEFT - CONV_BOX_BUF;
+CONV_COL_RIGHT = BOX_COL_RIGHT + CONV_BOX_BUF;
 
 
 end
@@ -145,15 +190,15 @@ end
 % % % hold on
 % % % plot(edges.selectStrongest(100));
 % % % pause
-hold on
-plot(x,y,'r.','MarkerSize',20) %%Draws redot of where user selected
+% hold on
+% plot(x,y,'r.','MarkerSize',20) %%Draws redot of where user selected
 
 %%% Counter Finding 
 %Need to round numbers for boundarytrace
-x = round(x);
-y = round(y);
-contour = bwtraceboundary(M,[y x],'W',8,Inf,'counterclockwise'); %Note x and y are flipped bc of r and col def
-plot(contour(:,2),contour(:,1),'g','LineWidth',2);
+% % x = round(x);
+% % y = round(y);
+% % contour = bwtraceboundary(M,[y x],'W',8,Inf,'counterclockwise'); %Note x and y are flipped bc of r and col def
+% % plot(contour(:,2),contour(:,1),'g','LineWidth',2);
 % v_new = v_gaus.* uint8(M);
 % v_new = 255 - v_new;
 % image(v_new);
